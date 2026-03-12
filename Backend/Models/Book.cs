e21d3a6b5d28553781770994cefa6d2c5a878930
isranjan <isranjansathiyaseelan@gmail.com>
2026-03-12 14:58:11 +0530

    Add project files.

---
+++ Backend/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿namespace Backend.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; } = null!;
+
+        public string Author { get; set; } = null!;
+
+        public string Isbn { get; set; } = null!;
+
+        public DateTime PublicationDate { get; set; } 
+    }
+}
