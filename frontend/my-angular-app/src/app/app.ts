358f757399ec4450d815413d5ecc9c40b6887475
isranjan <isranjansathiyaseelan@gmail.com>
2026-03-12 19:51:59 +0530

    frontend completed

---
+++ frontend/my-angular-app/src/app/app.ts
@@ -0,0 +1,70 @@
+import { Component, OnInit } from '@angular/core';
+import { BookService } from './book.service';
+import { Book } from './book';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App implements OnInit {
+
+  books: Book[] = [];
+  newBook: Book = { id: 0, title: '', author: '', isbn: '', publicationDate: '' };
+
+  editing: boolean = false;
+  selectedBookId: number | null = null;
+
+  constructor(private bookService: BookService) {}
+
+  ngOnInit(): void {
+    this.loadBooks();
+  }
+
+  loadBooks() {
+    this.bookService.getBooks().subscribe(data => {
+      this.books = data;
+    });
+  }
+
+  addBook() {
+    if (this.editing && this.selectedBookId !== null) {
+      this.bookService.updateBook(this.selectedBookId, this.newBook).subscribe(() => {
+        this.loadBooks();
+        this.cancelEdit();
+      });
+    } else {
+      this.bookService.addBook(this.newBook).subscribe(() => {
+        this.loadBooks();
+        this.resetForm();
+      });
+    }
+  }
+
+  editBook(book: Book) {
+    this.newBook = { ...book };
+    this.selectedBookId = book.id;
+    this.editing = true;
+  }
+
+  deleteBook(id: number) {
+    this.bookService.deleteBook(id).subscribe(() => {
+      this.loadBooks();
+      if (this.selectedBookId === id) this.cancelEdit();
+    });
+  }
+
+  cancelEdit() {
+    this.resetForm();
+    this.editing = false;
+    this.selectedBookId = null;
+  }
+
+  resetForm() {
+    this.newBook = { id: 0, title: '', author: '', isbn: '', publicationDate: '' };
+  }
+}
