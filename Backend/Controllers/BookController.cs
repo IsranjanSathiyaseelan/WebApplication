1467b3b538a7d8bfd89ff948cb39d2ac685835ff
isranjan <isranjansathiyaseelan@gmail.com>
2026-03-12 15:14:38 +0530

    backend completed

--- Backend/Controllers/BookController.cs
+++ Backend/Controllers/BookController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Backend.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace Backend.Controllers
 {
@@ -7,5 +9,69 @@ namespace Backend.Controllers
     [ApiController]
     public class BookController : ControllerBase
     {
+        private static List<Book> books = new List<Book>();
+
+        // GET: api/book
+        [HttpGet]
+        public ActionResult<IEnumerable<Book>> GetBooks()
+        {
+            return Ok(books);
+        }
+
+        // GET: api/book/1
+        [HttpGet("{id}")]
+        public ActionResult<Book> GetBook(int id)
+        {
+            var book = books.FirstOrDefault(b => b.Id == id);
+
+            if (book == null)
+                return NotFound();
+
+            return Ok(book);
+        }
+
+        // POST: api/book
+        [HttpPost]
+        public ActionResult<Book> AddBook(Book newBook)
+        {
+            if (newBook == null)
+                return BadRequest();
+
+            books.Add(newBook);
+
+            return CreatedAtAction(nameof(GetBook), new { id = newBook.Id }, newBook);
+        }
+
+        // PUT: api/book/1
+        [HttpPut("{id}")]
+        public IActionResult UpdateBook(int id, Book updatedBook)
+        {
+            var book = books.FirstOrDefault(b => b.Id == id);
+
+            if (book == null)
+                return NotFound();
+
+            book.Title = updatedBook.Title;
+            book.Author = updatedBook.Author;
+            book.Isbn = updatedBook.Isbn;
+            book.PublicationDate = updatedBook.PublicationDate;
+
+            return NoContent();
+        }
+
+        // DELETE: api/book/1
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBook(int id)
+        {
+            var book = books.FirstOrDefault(b => b.Id == id);
+
+            if (book == null)
+                return NotFound();
+
+            books.Remove(book);
+
+            return NoContent();
+        }
     }
 }
+
